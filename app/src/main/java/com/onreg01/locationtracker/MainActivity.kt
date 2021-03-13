4adb002862d3d582b939418775b3f9cdfd02d438
Vadzim <onreg01@gmail.com>
2021-03-13 15:46:01 +0300

    permission checks

--- app/src/main/java/com/onreg01/locationtracker/MainActivity.kt
+++ app/src/main/java/com/onreg01/locationtracker/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.onreg01.locationtracker
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.LocationSettingsRequest
 import com.onreg01.locationtracker.LocationTrackerService.Companion.locationRequest
 import com.onreg01.locationtracker.databinding.ActivityMainBinding
 import com.onreg01.locationtracker.db.DatabaseProvider
 import com.onreg01.locationtracker.db.log
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -24,6 +28,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val binding by viewBinding(ActivityMainBinding::bind, R.id.container)
 
+    private val activityResult = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        if (it) {
+            checkSettings()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -33,26 +43,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         binding.toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.start -> {
-                    LocationServices.getSettingsClient(this)
-                        .checkLocationSettings(
-                            LocationSettingsRequest.Builder()
-                                .addLocationRequest(locationRequest).build()
-                        ).addOnCompleteListener {
-                            if (it.isSuccessful) {
-                                ContextCompat.startForegroundService(this, Intent(this, LocationTrackerService::class.java))
-                            } else {
-                                it.exception?.let {
-                                    if (it is ResolvableApiException) {
-                                        it.startResolutionForResult(
-                                            this@MainActivity,
-                                            REQUEST_CHECK_SETTINGS
-                                        )
-                                    } else {
-                                        log("setting aren't resolvable ${it}")
-                                    }
-                                }
-                            }
-                        }
+                    checkOrRequestPermissions()
                 }
                 R.id.clear -> {
                     stopService(Intent(this, LocationTrackerService::class.java))
@@ -67,6 +58,53 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         DatabaseProvider.db.logDao()
             .getAllLogs()
             .onEach { adapter.submitList(it) }
+            .catch { log("Error observing logDao") }
             .launchIn(lifecycleScope)
+    }
+
+    private fun checkOrRequestPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            checkSettings()
+        } else {
+            activityResult.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+    }
+
+    private fun checkSettings() {
+        LocationServices.getSettingsClient(this)
+            .checkLocationSettings(
+                LocationSettingsRequest.Builder()
+                    .addLocationRequest(locationRequest).build()
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    ContextCompat.startForegroundService(this, Intent(this, LocationTrackerService::class.java))
+                } else {
+                    it.exception?.let {
+                        if (it is ResolvableApiException) {
+                            it.startResolutionForResult(
+                                this@MainActivity,
+                                REQUEST_CHECK_SETTINGS
+                            )
+                        } else {
+                            log("setting aren't resolvable ${it}")
+                        }
+                    }
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CHECK_SETTINGS && resultCode == Activity.RESULT_OK) {
+            ContextCompat.startForegroundService(this, Intent(this, LocationTrackerService::class.java))
+        }
     }
 }
